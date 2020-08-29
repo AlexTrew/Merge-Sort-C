6c8154a07c4bbb74ab80811aee19ce40213a5408
alextrew <alextrew@protonmail.com>
2020-08-29 11:53:30 +0100

    added missing return statements and added main loop and quit option

--- mergesort.cpp
+++ mergesort.cpp
@@ -36,8 +36,8 @@ std::vector<int> Merge(const std::vector<int>& l, const std::vector<int>& r) {
 }
 
 std::vector<int> MergeSort(const std::vector<int>& arr) {
-	if (arr.size() > 1){
-		size_t middle = arr.size()/2;
+	if (arr.size() > 1) {
+		size_t middle = arr.size() / 2;
 		std::vector<int> split_lo(arr.begin(), arr.begin() + middle);
 		std::vector<int> split_hi(arr.begin() + middle, arr.end());
 		return Merge(MergeSort(split_lo), MergeSort(split_hi));
@@ -46,28 +46,37 @@ std::vector<int> MergeSort(const std::vector<int>& arr) {
 }
 
 int main() {
+	while (true) {
+		std::cout << "Enter a sequence of numbers, separated by spaces. Type \"q\" to quit:" << std::endl;
 
-	std::cout << "Enter a sequence of numbers, separated by spaces to be sorted" << std::endl;
+		std::string input;
+		std::getline(std::cin, input);
+		if (std::regex_match(input, std::regex("([0-9]+\\ )+[0-9]+")) || input.compare("q") == 0) {
+			if (input.compare("q") == 0) {
+				return 1;
+			}
 
-	std::string input;
-	std::getline(std::cin, input);
+			std::istringstream iss(input);
+			std::vector<std::string> numbers((std::istream_iterator<std::string>(iss)), std::istream_iterator<std::string>());
 
-	if (!std::regex_match(input, std::regex("([0-9]+\\ )+[0-9]+"))) {
-		std::cout << "input format error" << std::endl;
+			std::vector<int> sort_me;
+			for (std::string& number : numbers) {
+				sort_me.push_back(std::stoi(number));
+			}
+
+			std::vector<int> sorted = MergeSort(sort_me);
+			std::string output;
+			for (int num : sorted) {
+				output = output + std::to_string(num) + " ";
+			}
+			std::cout << output << std::endl;
+
+		}
+		else {
+			std::cout << "input format error" << std::endl;
+		}
+
 	}
+	return 0;
 
-	std::istringstream iss(input);
-	std::vector<std::string> numbers((std::istream_iterator<std::string>(iss)), std::istream_iterator<std::string>());
-
-	std::vector<int> sort_me;
-	for (std::string& number : numbers) {
-		sort_me.push_back(std::stoi(number));
-	}
-
-	std::vector<int> sorted = MergeSort(sort_me);
-	std::string output;
-	for (int num : sorted) {
-		output = output + std::to_string(num) + " ";
-	}
-	std::cout << output << std::endl;
 }
